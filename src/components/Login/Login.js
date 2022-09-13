d8da364e4119c7b7f20a6e1065ca5d3f606ce827
JooJo <sjak0101@gmail.com>
2022-09-13 16:48:55 +0100

    added onauthstate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h2>Please Login</h2>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign in</button>
+            </div>
+            <form action="">
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" name='' id='' placeholder='password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
